67973fb953661e6d581299cefa13f4db7c6fcd3c
Djumabaevs <djumabaevb@gmail.com>
2021-10-10 00:32:11 +0600

    override on create view

--- app/src/main/java/com/djumabaevs/gochipapp/PetsListFragment.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/PetsListFragment.kt
@@ -5,22 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.djumabaevs.gochipapp.databinding.FragmentPetsListBinding
 
 
 class PetsListFragment : Fragment() {
 
-
+private lateinit var binding: FragmentPetsListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pets_list, container, false)
+    ): View {
+        binding = FragmentPetsListBinding.inflate(inflater)
+        return binding.root
     }
 
 
