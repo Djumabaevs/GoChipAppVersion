daa428f27d3af272927af354a67333bf7548a0ff
Djumabaevs <djumabaevb@gmail.com>
2021-10-10 00:27:04 +0600

    fragment list of pets

---
+++ app/src/main/java/com/djumabaevs/gochipapp/PetsListAdapter.kt
@@ -0,0 +1,22 @@
+package com.djumabaevs.gochipapp
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PetsListAdapter(
+    private val pets: List<GetAllPetsQuery.Pet>
+) : RecyclerView.Adapter<PetsListAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PetsListAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PetsListAdapter.ViewHolder, position: Int) {
+        val pet = pets[position]
+        holder.binding.site.text = launch.site ?: ""
+    }
+
+    override fun getItemCount(): Int {
+        return pets.size
+    }
+
+}
