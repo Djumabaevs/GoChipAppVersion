04cc68bb8787f29d1fea47b7f972ff8ce49df72e
Djumabaevs <djumabaevb@gmail.com>
2021-10-12 18:50:30 +0600

    add rx ble dependencies

--- app/src/main/java/com/djumabaevs/gochipapp/DevicesFragment.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/DevicesFragment.kt
@@ -1,14 +1,22 @@
 package com.djumabaevs.gochipapp
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import dagger.hilt.android.AndroidEntryPoint
+import android.bluetooth.BluetoothManager
+import androidx.appcompat.app.AppCompatActivity
+import com.vincentmasselis.rxbluetoothkotlin.rxScan
+import com.vincentmasselis.rxuikotlin.disposeOnState
+import com.vincentmasselis.rxuikotlin.utils.ActivityState
+import com.vincentmasselis.rxuikotlin.postForUI
 
 
 class DevicesFragment : Fragment() {
@@ -27,6 +35,18 @@ class DevicesFragment : Fragment() {
         devicesViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
+
+
+        (getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager)
+            .rxScan()
+            .subscribe {
+                applicationContext
+            }
+            .disposeOnState(ActivityState.DESTROY, this)
         return root
+    }
+
+    fun setMessage(message: String) {
+        postForUI {  = message }
     }
 }
