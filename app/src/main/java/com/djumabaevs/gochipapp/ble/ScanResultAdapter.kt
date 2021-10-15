d0c23a71f905a36ec13dda64c5081240014c7be7
Djumabaevs <djumabaevb@gmail.com>
2021-10-15 09:32:15 +0600

    override methods in adapter

--- app/src/main/java/com/djumabaevs/gochipapp/ble/ScanResultAdapter.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/ble/ScanResultAdapter.kt
@@ -17,3 +17,26 @@ class ScanResultAdapter(private val inflater: LayoutInflater, private val recycl
         scanResults += scanResult
         notifyItemInserted(scanResults.size - 1)
     }
+    override fun getItemCount(): Int = scanResults.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScanResultViewHolder = ScanResultViewHolder(inflater.inflate(R.layout.cell_scan_result, parent, false))
+
+    override fun onViewAttachedToWindow(holder: ScanResultViewHolder) {
+        super.onViewAttachedToWindow(holder)
+        holder.itemView.setOnClickListener(this)
+    }
+
+    override fun onBindViewHolder(holder: ScanResultViewHolder, position: Int) {
+        val scanResult = scanResults[position]
+        holder.bind(scanResult.device.name ?: "N/A", scanResult.device.address)
+    }
+
+    override fun onViewDetachedFromWindow(holder: ScanResultViewHolder) {
+        holder.itemView.setOnClickListener(null)
+        super.onViewDetachedFromWindow(holder)
+    }
+
+    override fun onClick(view: View) {
+        recyclerView.context.startActivity(DeviceActivity.intent(recyclerView.context, scanResults[recyclerView.getChildAdapterPosition(view)].device))
+    }
+}
