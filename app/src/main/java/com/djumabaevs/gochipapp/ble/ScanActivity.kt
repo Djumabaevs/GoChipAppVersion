92d0f3ad7acb230574a3424cc0a3b6dd2fcf61cd
Djumabaevs <djumabaevb@gmail.com>
2021-10-15 09:55:17 +0600

    behaviour subject

--- app/src/main/java/com/djumabaevs/gochipapp/ble/ScanActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/ble/ScanActivity.kt
@@ -23,8 +23,33 @@ import kotlinx.android.synthetic.main.activity_scan.*
 import java.util.concurrent.TimeUnit
 
 class ScanActivity : AppCompatActivity() {
+
+    private var currentState = BehaviorSubject.createDefault<States>(States.NotScanning)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-    }
-}
+
+        currentState
+            .distinctUntilChanged()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                @Suppress("UNUSED_VARIABLE") val ignoreMe = when (it) {
+                    States.NotScanning -> {
+                        not_scanning_group.visibility = View.VISIBLE
+                        start_scan_group.visibility = View.GONE
+                        scanning_group.visibility = View.GONE
+                    }
+                    States.StartingScan -> {
+                        not_scanning_group.visibility = View.GONE
+                        start_scan_group.visibility = View.VISIBLE
+                        scanning_group.visibility = View.GONE
+                    }
+                    States.Scanning -> {
+                        not_scanning_group.visibility = View.GONE
+                        start_scan_group.visibility = View.GONE
+                        scanning_group.visibility = View.VISIBLE
+                    }
+                }
+            }
+            .disposeOnState(ActivityState.DESTROY, this)
