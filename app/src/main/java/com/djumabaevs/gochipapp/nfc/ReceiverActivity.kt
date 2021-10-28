3696654d14c59e7c5bbaad2983f624ab47bfc20b
Djumabaevs <djumabaevb@gmail.com>
2021-10-28 09:50:54 +0600

    work manager enqueue sample work

--- app/src/main/java/com/djumabaevs/gochipapp/nfc/ReceiverActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/nfc/ReceiverActivity.kt
@@ -12,10 +12,7 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.Constraints
-import androidx.work.Data
-import androidx.work.NetworkType
-import androidx.work.OneTimeWorkRequest
+import androidx.work.*
 import com.djumabaevs.gochipapp.R
 import com.djumabaevs.gochipapp.util.OneTimeRequestWorker
 
@@ -68,6 +65,9 @@ class ReceiverActivity : AppCompatActivity() {
                 .Builder(OneTimeRequestWorker::class.java)
                 .setInputData(data.build())
                 .setConstraints(oneTimeRequestConstraints)
+                .build()
+
+            WorkManager.getInstance(this).enqueue(sampleWork)
         }
     }
 
