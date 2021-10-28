1ba90ddd5c201447bdf0f62efe50a333604e31db
Djumabaevs <djumabaevb@gmail.com>
2021-10-28 13:20:08 +0600

    get date every hour

--- app/src/main/java/com/djumabaevs/gochipapp/util/PeriodicRequestWorker.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/util/PeriodicRequestWorker.kt
@@ -3,6 +3,8 @@ package com.djumabaevs.gochipapp.util
 import android.content.Context
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PeriodicRequestWorker(context: Context, params: WorkerParameters): Worker(context, params) {
 
@@ -10,4 +12,10 @@ class PeriodicRequestWorker(context: Context, params: WorkerParameters): Worker(
 
     }
 
+    private fun getDate(milliSeconds: Long) : String {
+        val formatter = SimpleDateFormat("dd/MM/yyyy hh:mm:ss.SSS", Locale.getDefault())
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = milliSeconds
+        return formatter.format(calendar.time)
+    }
 }
