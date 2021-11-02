9a63d26354e6d94ec246b7b8090ba952a821e1ce
Djumabaevs <djumabaevb@gmail.com>
2021-11-02 13:10:06 +0600

    firebaseAuth

--- app/src/main/java/com/djumabaevs/gochipapp/login/LoginActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/login/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.djumabaevs.gochipapp.login
 
+import android.app.ProgressDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.djumabaevs.gochipapp.MainActivity
 import com.djumabaevs.gochipapp.R
 import com.djumabaevs.gochipapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.PhoneAuthProvider
 
 class LoginActivity : AppCompatActivity() {
@@ -16,13 +19,23 @@ class LoginActivity : AppCompatActivity() {
     private var forceResendingToken: PhoneAuthProvider.ForceResendingToken? = null
 
     private var mCallBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks? = null
+    private var mVerificationId: String? = null
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private val TAG = "MAIN_TAG"
+
+    private lateinit var progressDialog: ProgressDialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.phoneLl.visibility = View.VISIBLE
+        binding.codeLl.visibility = View.GONE
 
+        firebaseAuth = FirebaseAuth.getInstance()
 
 //        var btnToDrawer = findViewById<Button>(R.id.go_to_drawer)
 //        btnToDrawer.setOnClickListener {
