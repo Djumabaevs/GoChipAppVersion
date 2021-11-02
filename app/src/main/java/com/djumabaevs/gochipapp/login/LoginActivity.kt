b01789863d4e0c2793560713ac798f97511686f9
Djumabaevs <djumabaevb@gmail.com>
2021-11-02 13:05:28 +0600

    PhoneAuthProvider

--- app/src/main/java/com/djumabaevs/gochipapp/login/LoginActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/login/LoginActivity.kt
@@ -6,18 +6,29 @@ import android.os.Bundle
 import android.widget.Button
 import com.djumabaevs.gochipapp.MainActivity
 import com.djumabaevs.gochipapp.R
+import com.djumabaevs.gochipapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.PhoneAuthProvider
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
+    private var forceResendingToken: PhoneAuthProvider.ForceResendingToken? = null
+
+    private var mCallBacks: PhoneAuthProvider.OnVerificationStateChangedCallbacks? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var btnToDrawer = findViewById<Button>(R.id.go_to_drawer)
-        btnToDrawer.setOnClickListener {
-            var intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
 
-        }
+
+//        var btnToDrawer = findViewById<Button>(R.id.go_to_drawer)
+//        btnToDrawer.setOnClickListener {
+//            var intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//
+//        }
     }
 }
