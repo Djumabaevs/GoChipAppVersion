82757286d638b9a020e5e3164e07ba53d6b4fe81
Djumabaevs <djumabaevb@gmail.com>
2021-11-23 00:40:22 +0600

    added multiple language support

--- app/src/main/java/com/djumabaevs/gochipapp/login/newLogin/ChooseProfileActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/login/newLogin/ChooseProfileActivity.kt
@@ -3,6 +3,7 @@ package com.djumabaevs.gochipapp.login.newLogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.djumabaevs.gochipapp.MainActivity
 import com.djumabaevs.gochipapp.R
 import com.djumabaevs.gochipapp.databinding.ActivityChooseProfileBinding
@@ -15,6 +16,11 @@ class ChooseProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+            setTheme(R.style.darkTheme) //when dark mode is enabled, we use the dark theme
+        } else {
+            setTheme(R.style.AppTheme)  //default app theme
+        }
         binding = ActivityChooseProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
