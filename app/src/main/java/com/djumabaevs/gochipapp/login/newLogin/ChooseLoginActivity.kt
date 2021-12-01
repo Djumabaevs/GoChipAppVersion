f3da6f621be21dd42d7daacfffe4f8ff28ee8785
Djumabaevs <djumabaevb@gmail.com>
2021-12-01 15:20:47 +0600

    keycloak auth

--- app/src/main/java/com/djumabaevs/gochipapp/login/newLogin/ChooseLoginActivity.kt
+++ app/src/main/java/com/djumabaevs/gochipapp/login/newLogin/ChooseLoginActivity.kt
@@ -3,10 +3,23 @@ package com.djumabaevs.gochipapp.login.newLogin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.djumabaevs.gochipapp.R
+import com.djumabaevs.gochipapp.databinding.ActivityChooseLoginBinding
 
 class ChooseLoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityChooseLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityChooseLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_login)
+        setContentView(binding.root)
+
+        binding.loginKeycloak.setOnClickListener {
+
+        }
+
+        binding.loginEmailAuth.setOnClickListener {
+
+        }
     }
 }
