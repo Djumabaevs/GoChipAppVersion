062d1bdac8eae0462b805185507aa6ac27e70f1d
Djumabaevs <djumabaevb@gmail.com>
2021-12-01 16:05:04 +0600

    keycloak activity

---
+++ app/src/main/java/com/djumabaevs/gochipapp/login/newLogin/KeycloakActivity.kt
@@ -0,0 +1,12 @@
+package com.djumabaevs.gochipapp.login.newLogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.djumabaevs.gochipapp.R
+
+class KeycloakActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_keycloak)
+    }
+}
