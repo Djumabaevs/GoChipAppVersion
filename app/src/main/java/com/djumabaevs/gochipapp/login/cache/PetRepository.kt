858bc03f80174194269087c9070e8ad6eb8aa168
Djumabaevs <djumabaevb@gmail.com>
2021-12-04 10:40:16 +0600

    pet repository

---
+++ app/src/main/java/com/djumabaevs/gochipapp/login/cache/PetRepository.kt
@@ -0,0 +1,28 @@
+package com.djumabaevs.gochipapp.login.cache
+
+import androidx.room.withTransaction
+import kotlinx.coroutines.delay
+import javax.inject.Inject
+
+class RestaurantRepository @Inject constructor(
+    private val api: RestaurantApi,
+    private val db: RestaurantDatabase
+) {
+    private val restaurantDao = db.restaurantDao()
+
+    fun getRestaurants() = networkBoundResource(
+        query = {
+            restaurantDao.getAllRestaurants()
+        },
+        fetch = {
+            delay(2000)
+            api.getRestaurants()
+        },
+        saveFetchResult = { restaurants ->
+            db.withTransaction {
+                restaurantDao.deleteAllRestaurants()
+                restaurantDao.insertRestaurants(restaurants)
+            }
+        }
+    )
+}
